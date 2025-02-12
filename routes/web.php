a209fb137710be9ba825fb8198775e917bd73709
Rohit Rawat <rohitrawat676@gmail.com>
2025-02-12 15:37:09 +0530

    Office Order Done

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Auth;
-use App\Helpers\Helper;
-=======
 use App\Helpers\Helper;
 use Illuminate\Support\Facades\Auth;
-
->>>>>>> rohitdev
 
 /*
 |--------------------------------------------------------------------------
@@ -118,4 +112,14 @@ Route::group(['namespace' => 'admin', 'prefix' => 'admin', 'as' => 'admin.'], fu
     Route::get('/admin/document/office_order/get-divisions-by-user', [App\Http\Controllers\OfficeOrder::class, 'getDivisionsByUser'])->name('document.office_order.get-divisions-by-user');
     Route::delete('/admin/document/office_order/delete_file', [App\Http\Controllers\OfficeOrder::class, 'deleteFile'])->name('document.office_order.delete_file');
 
+
+    Route::get('/document/notification',[App\Http\Controllers\NotificationController::class, 'notification'])->name('document.notification.index');
+    Route::match(['get','post'],'/document/notification/create',[App\Http\Controllers\NotificationController::class, 'create'])->name('document.notification.create');
+    Route::match(['get','post'],'/document/notification/edit/{id}',[App\Http\Controllers\NotificationController::class, 'edit'])->name('document.notification.edit');
+    Route::post('/document/notification/status',[App\Http\Controllers\NotificationController::class, 'status'])->name('document.notification.status');
+    Route::post('/notification/delete',[App\Http\Controllers\NotificationController::class, 'destroy'])->name('notification.delete');
+    Route::get('view-pdf/{file}', [App\Http\Controllers\NotificationController::class, 'viewPdf'])->name('view.pdf');
+    Route::get('/admin/document/notification/get-divisions-by-user', [App\Http\Controllers\NotificationController::class, 'getDivisionsByUser'])->name('document.notification.get-divisions-by-user');
+    Route::delete('/admin/document/notification/delete_file', [App\Http\Controllers\NotificationController::class, 'deleteFile'])->name('document.notification.delete_file');
+    
 });
